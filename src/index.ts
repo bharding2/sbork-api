da83524a2559d8001c1336402dc8cbae79349b84
bharding2 <b.harding@gmail.com>
2020-03-23 12:40:49 -0700

    adding cors and postgres connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response, Application } from 'express';
+import cors from 'cors';
+import pgPromise from 'pg-promise';
+
 const app: Application = express();
 const port = process.env.API_PORT;
+
+const pgp = pgPromise();
+const db = pgp(process.env.SBORK_PG_CONNECTION_STRING);
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello world!");
