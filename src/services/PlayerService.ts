60a8630b4334477d5d4b344912b4ce036ef8b5c1
bharding2 <b.harding@gmail.com>
2020-04-13 18:23:03 -0700

    sending sample player from backend

--- src/services/PlayerService.ts
+++ src/services/PlayerService.ts
@@ -16,10 +16,28 @@ export interface Player {
     shortShuttle?: number;
 }
 
+const samplePlayer: Player = {
+    id: '0',
+    name: 'sample player',
+    dob: '1980-01-01',
+    college: 'University of Slothbear',
+    draftYear: '2000',
+    position: 'cb',
+    height: 5.77,
+    weight: 177,
+    fortyYard: 4.5,
+    tenYard: 1.45,
+    benchPress: 15,
+    verticalJump: 40,
+    broadJump: 11,
+    threeCone: 6.56,
+    shortShuttle: 4.06
+};
+
 export class PlayerService {
     static getAll = (): Promise<Player[]> => {
         return new Promise((resolve, reject) => {
-            resolve([]);
+            resolve([samplePlayer]);
         });
     }
 }
